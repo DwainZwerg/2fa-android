11a7aa0db681c0ed054d030eeb541974447973be
Babkov <babkov@979.st>
2025-10-04 14:42:13 +0200

    Increment version to Alpha 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
     defaultConfig {
         minSdk = 35
         targetSdk = 36
-        versionCode = 8
-        versionName = "Alpha 8"
+        versionCode = 9
+        versionName = "Alpha 9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
