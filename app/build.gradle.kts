ec79b92697facd9fc7a75eea94498cc57232f17f
Babkov <babkov@979.st>
2025-10-17 15:38:41 +0200

    Increment version to Beta 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
     defaultConfig {
         minSdk = 35
         targetSdk = 36
-        versionCode = 11
-        versionName = "Beta 2"
+        versionCode = 12
+        versionName = "Beta 3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
